aa572ae6328a0fb11ff74afb6e04f76f85739270
Betheony <bt.bethanyt@gmail.com>
2025-01-25 17:09:07 -0600

    added all outer buttons nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,23 @@ const App = () => {
       <Router>
         {/* Dropdown Menu */}
         <div className="absolute p-4">
-          <button
-            onClick={toggleDropdown}
-            className="bg-[url('./assets/menubutton.png')] w-18 h-19 items-center justify-center bg-center bg-center rounded-md"
-          >
-          </button>
+          <div className='flex flex-row space-x-4'>
+            <button
+              onClick={toggleDropdown}
+              className="bg-[url('./assets/menubutton.png')] w-18 h-19 items-center justify-center bg-center bg-center rounded-md"
+            >
+            </button>
+            <Link
+              to="/budget"
+              className="bg-[url('./assets/budgetbutton.png')] w-18 h-19 items-center justify-center bg-center bg-center rounded-md"
+            >
+            </Link>
+            <Link
+              to="/Gacha-Shop"
+              className="bg-[url('./assets/shopbutton.png')] w-18 h-19 items-center justify-center bg-center bg-center rounded-md"
+            >
+            </Link>
+          </div>
           {dropdownOpen && (
             <div className="absolute mt-2 w-40 bg-white shadow-lg rounded-md">
               <Link
@@ -29,20 +41,6 @@ const App = () => {
                 onClick={() => setDropdownOpen(false)}
               >
                 Home
-              </Link>
-              <Link
-                to="/budget"
-                className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                onClick={() => setDropdownOpen(false)}
-              >
-                Budget
-              </Link>
-              <Link
-                to="/gacha-shop"
-                className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
-                onClick={() => setDropdownOpen(false)}
-              >
-                Gacha Shop
               </Link>
             </div>
           )}
