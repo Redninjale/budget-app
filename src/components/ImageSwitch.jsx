a0c05e19dd89e9944d87bb3b293d5987a7478afe
Summer Wong <113924489+sumshiu@users.noreply.github.com>
2025-01-26 06:21:18 -0600

    updated to pop up

--- src/components/ImageSwitch.jsx
+++ src/components/ImageSwitch.jsx
@@ -5,9 +5,16 @@ const GifSwitcher = ({ initialGif, clickedGif, delay }) => {
   // State to track the current GIF
   const [gif, setGif] = useState(initialGif);
 
+  const [showPopup, setShowPopup] = useState(false);
+
   // Function to change the GIF on click
   const changeGif = () => {
     setGif(clickedGif); // Change to the clicked GIF
+    setShowPopup(false); // Reset popup visibility
+    // Show the popup after 5 seconds
+    setTimeout(() => {
+      setShowPopup(true);
+    }, 4500); // 5000ms = 5 seconds
   };
 
   // Revert the GIF back after 3 seconds
@@ -29,10 +36,16 @@ const GifSwitcher = ({ initialGif, clickedGif, delay }) => {
       className="cursor-pointer"
       alt="GIF"
     />
+
     <button className="!bg-transparent !border-none flex items-center justify-center " onClick={changeGif}>
       <img src={gachaButton} alt="gacha button" className="flex items-center justify-center bg-transparent w-3/4" />
-
-    </button></div>
+    </button>
+    {showPopup && (
+        <div className="absolute top-1/3 left-1/10 bg-white border rounded-lg shadow-lg p-20">
+          <p className="text-center text-gray-800">This is the popup!</p>
+        </div>
+      )}
+    </div>
   );
 };
 
