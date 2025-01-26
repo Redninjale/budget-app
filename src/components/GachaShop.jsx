686c7a1f889d9b9d22131845ff55560ac7ff6b59
Betheony <bt.bethanyt@gmail.com>
2025-01-26 01:44:12 -0600

    fixed nav bar and shop

--- src/components/GachaShop.jsx
+++ src/components/GachaShop.jsx
@@ -6,22 +6,20 @@ const GachaShop = () => {
   const [view, setView] = useState('gacha');
 
   return (
-    <div className="p-4 pt-16 bg-[url('./assets/gachabackground.png')] bg-cover flex h-screen w-screen">
-      <div className="flex justify-center space-x-4 absolute ms-15 top-30">
+    <div className="flex flex-col pt-29 bg-[url('./assets/gachabackground.png')] bg-cover flex h-screen w-screen">
+      <div className="flex flex-row justify-center space-x-4">
         <button
           onClick={() => setView('gacha')}
-          className={`py-2 px-4 rounded-lg ${view === 'gacha' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          className={`bg-[url('./assets/shopbutton.png')] w-18 h-19 rounded-lg ${view === 'gacha' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
         >
-          Gacha
         </button>
         <button
           onClick={() => setView('shop')}
-          className={`py-2 px-4 rounded-lg ${view === 'shop' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          className={`bg-[url('./assets/shopbutton2.png')] w-18 h-19 rounded-lg ${view === 'shop' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
         >
-          Shop
         </button>
       </div>
-      <div className="mt-6">
+      <div className="mt-2">
         {view === 'gacha' ? <Gacha /> : <Shop />}
       </div>
     </div>
--- src/components/GachaShop/Shop.jsx
+++ src/components/GachaShop/Shop.jsx
@@ -4,20 +4,19 @@ const Shop = () => {
   const items = ['Bow Tie', 'Top Hat', 'Flower Crown'];
 
   return (
-    // <div>
-    //   <h1 className="text-2xl font-bold text-center">Shop Accessories</h1>
-    //   <div className="grid grid-cols-3 gap-4 mt-6">
-    //     {items.map((item) => (
-    //       <div
-    //         key={item}
-    //         className="py-4 px-4 bg-gray-100 text-center rounded-lg shadow-md"
-    //       >
-    //         {item}
-    //       </div>
-    //     ))}
-    //   </div>
-    // </div>
-    <h1>hello</h1>
+    <div>
+      <h1 className="text-2xl font-bold text-center">Shop Accessories</h1>
+      <div className="grid grid-cols-3 gap-4 mt-6">
+        {items.map((item) => (
+          <div
+            key={item}
+            className="py-4 px-4 bg-gray-100 text-center rounded-lg shadow-md"
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
