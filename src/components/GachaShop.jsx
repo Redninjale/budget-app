98cbbe29a085327df145e9a2894c9b1ffc92e070
Betheony <bt.bethanyt@gmail.com>
2025-01-26 02:11:04 -0600

    shop and gacha buttons and stripe

--- src/components/GachaShop.jsx
+++ src/components/GachaShop.jsx
@@ -2,22 +2,27 @@ import React, { useState } from 'react';
 import Gacha from './GachaShop/Gacha';
 import Shop from './GachaShop/Shop';
 
+import shopbutton from '../assets/shopbutton.png';
+import shopbutton2 from '../assets/shopbutton2.png';
+
 const GachaShop = () => {
   const [view, setView] = useState('gacha');
 
   return (
-    <div className="flex flex-col pt-29 bg-[url('./assets/gachabackground.png')] bg-cover flex h-screen w-screen">
+    <div className="flex flex-col pt-29 bg-[url('./assets/greenstripe.gif')] bg-cover flex h-screen w-screen">
       <div className="flex flex-row justify-center space-x-4">
-        <button
+        <img
+        src={shopbutton}
           onClick={() => setView('gacha')}
-          className={`bg-[url('./assets/shopbutton.png')] w-18 h-19 rounded-lg ${view === 'gacha' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          className={`w-18 h-19 rounded-lg`}
         >
-        </button>
-        <button
+        </img>
+        <img
+        src={shopbutton2}
           onClick={() => setView('shop')}
-          className={`bg-[url('./assets/shopbutton2.png')] w-18 h-19 rounded-lg ${view === 'shop' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          className={`w-18 h-19 rounded-lg`}
         >
-        </button>
+        </img>
       </div>
       <div className="mt-2">
         {view === 'gacha' ? <Gacha /> : <Shop />}
--- src/components/GachaShop/Shop.jsx
+++ src/components/GachaShop/Shop.jsx
@@ -5,12 +5,12 @@ const Shop = () => {
 
   return (
     <div>
-      <h1 className="text-2xl font-bold text-center">Shop Accessories</h1>
+      <h1 className="special-text text-2xl font-bold text-center">Shop Accessories</h1>
       <div className="grid grid-cols-3 gap-4 mt-6">
         {items.map((item) => (
           <div
             key={item}
-            className="py-4 px-4 bg-gray-100 text-center rounded-lg shadow-md"
+            className="special-text py-4 px-4 bg-gray-100 text-center rounded-lg shadow-md"
           >
             {item}
           </div>
