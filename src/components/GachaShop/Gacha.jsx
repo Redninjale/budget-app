ed187eb36bbf8b36e084ae7195416ead60ad30c4
Summer Wong <113924489+sumshiu@users.noreply.github.com>
2025-01-25 20:26:49 -0600

    not working

--- src/components/GachaShop/Gacha.jsx
+++ src/components/GachaShop/Gacha.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import bgImage from './assets/background.jpg';
+import bgImage from '../../assets/menubutton.png';
 
 const Gacha = () => {
+  console.log("fljeiojf");
   return (
-    <div
-      className="w-screen h-screen text-center overflow-hidden"
-      style={{
-        backgroundImage: `url(${bgImage})`,
-        backgroundSize: 'cover', // Ensures the image covers the container
-        backgroundPosition: 'center', // Centers the image
-        backgroundRepeat: 'no-repeat', // Prevents tiling if the image is smaller
-      }}
-    >
-      <div className="relative z-10 flex flex-col items-center justify-center h-full">
-        <h1 className="text-2xl font-bold text-white">Gacha Machine</h1>
-        <p className="text-gray-200 mt-2">Roll for a chance to collect new cats!</p>
-        <button className="mt-4 py-2 px-6 bg-yellow-400 text-white rounded-lg">Roll</button>
-      </div>
-    </div>
+    <main className="bg-[url('./assets/test.png')] bg-cover flex h-screen w-screen">
+        {/* <div className="relative z-10 flex flex-col items-center justify-center h-full">
+
+        </div> */}
+    </main>
+
+    // <div
+    //   className="w-screen h-screen text-center overflow-hidden bg-cover"
+    //   style={{
+    //     backgroundImage: `url(${bgImage})`,
+    //     backgroundSize: 'cover', // Ensures the image covers the container
+    //     backgroundPosition: 'center', // Centers the image
+    //     backgroundRepeat: 'no-repeat', // Prevents tiling if the image is smaller
+    //   }}
+    // >
+    //   <div className="relative z-10 flex flex-col items-center justify-center h-full">
+    //     <h1 className="text-2xl font-bold text-white">Gacha Machine</h1>
+    //     <p className="text-gray-200 mt-2">Roll for a chance to collect new cats!</p>
+    //     <button className="mt-4 py-2 px-6 bg-yellow-400 text-white rounded-lg">Roll</button>
+    //   </div>
+    // </div>
   );
 };
 
