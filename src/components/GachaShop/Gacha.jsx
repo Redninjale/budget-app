f7be897b442defade65e3230e7e2330505793e07
Summer Wong <113924489+sumshiu@users.noreply.github.com>
2025-01-26 07:15:04 -0600

    currency is removed

--- src/components/GachaShop/Gacha.jsx
+++ src/components/GachaShop/Gacha.jsx
@@ -16,10 +16,10 @@ const Gacha = () => {
       <ImageSwitchGacha initialGif={gachaMachine} clickedGif={gachaMachine1} delay={4600}></ImageSwitchGacha>
     {/* </button> */}
     </div>
-    <div className="relative w-full h-20">
+    {/* <div className="relative w-full h-20">
       <img src={coin} alt="coin" className="absolute bottom-7 left-0 m-2 w-15 h-15" />
       <p className='flex text-[50px] absolute bottom-8 left-17 text-white [text-shadow:2px_2px_0px_black] special-text'>100</p> 
-    </div>
+    </div> */}
 
       {/* <img src={gachaMachine} alt="gacha machine" className="w-1/2 h-1/2" /> */}
     </main>
